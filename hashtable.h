a8e41c5c623a5fbc586d021defb90640e51e5fef
ericboyd <epboyd@gmail.com>
2015-03-12 18:09:21 -0400

    hash table

--- hashtable.h
+++ hashtable.h
@@ -6,5 +6,7 @@ typedef struct HashTable HashTable;
 
 HashTable *hash_table_create(unsigned int size, void (*delete)(void *), unsigned int (*hash)(void *));
 void hash_table_destroy(HashTable *h);
+void hash_table_insert(HashTable *h, void *key, void *data);
+void *hash_table_retrieve(HashTable *h, void *key);
 
 #endif
