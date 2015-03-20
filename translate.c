76c33710cefb0df3e22fc4e050be6f596d7186ad
Murray Rush <rush@mail.uoguelph.ca>
2015-03-20 19:05:07 -0400

    type promotions?

--- translate.c
+++ translate.c
@@ -5,44 +5,60 @@
 int translate_expr(Node *expr){
 	if (strcmp(expr.operator.type, "+") == 0) || (strcmp(expr.operator.type, "-") == 0) || (strcmp(expr.operator.type, "*") == 0) ||
 		(strcmp(expr.operator.type, "/") == 0){
+
 		int left_expr = translate_expr(expr.left_expr);
 		int right_expr = translate_expr(expr.right_expr);
 
+		if (left_expr != T_INT) &&  (left_expr != T_REAL){
+			// ERROR
+		}
+
+		if (right_expr != T_INT) && (right_expr != T_REAL){
+			// ERROR
+		}
+
+		if (right_expr == T_REAL) || (left_expr == T_REAL){
+			// TYPE PROMOTION
+			return T_REAL;
+		}
 	}
 	else if (strcmp(expr.operator.type, "%") == 0){
-
+		if (right_expr == T_INT) && (left_expr == T_INT){
+			return T_INT;
+		}
+		else{
+			// ERROR
+		}
 	}
-	else if (strcmp(expr.operator.type, "==") == 0){
+	else if (strcmp(expr.operator.type, "==") == 0) || (strcmp(expr.operator.type, "!=") == 0) || (strcmp(expr.operator.type, ">=") == 0) ||
+		(strcmp(expr.operator.type, "<=") == 0) || (strcmp(expr.operator.type, ">") == 0) || (strcmp(expr.operator.type, "<") == 0){
 
-	}
-	else if (strcmp(expr.operator.type, "!=") == 0){
+		if (left_expr != T_INT) &&  (left_expr != T_REAL){
+			// ERROR
+		}
 
-	}
-	else if (strcmp(expr.operator.type, ">=") == 0){
+		if (right_expr != T_INT) && (right_expr != T_REAL){
+			// ERROR
+		}
 
+		if (right_expr == T_REAL) || (left_expr == T_REAL){
+			// TYPE PROMOTION
+		}
+		return T_INT;
 	}
-	else if (strcmp(expr.operator.type, "<=") == 0){
-
-	}
-	else if (strcmp(expr.operator.type, ">") == 0){
-
-	}
-	else if (strcmp(expr.operator.type, "<") == 0){
-
-	}
-	else if (strcmp(expr.operator.type, "&&") == 0){
-
-	}
-	else if (strcmp(expr.operator.type, "||") == 0){
+	else if (strcmp(expr.operator.type, "&&") == 0) ||  (strcmp(expr.operator.type, "||") == 0){
 
 	}
 	else if (strcmp(expr.operator.type, "!") == 0){
 
 	}
-	else if (strcmp(expr.operator.type, "--") == 0){
+	else if (strcmp(expr.operator.type, "--") == 0) || (strcmp(expr.operator.type, "++") == 0){
+		if (left_expr != T_INT) &&  (left_expr != T_REAL) && (left_expr != NULL){
+			// ERROR
+		}
 
-	}
-	else if (strcmp(expr.operator.type, "++") == 0){
-
+		if (right_expr != T_INT) && (right_expr != T_REAL) && (right_expr != NULL){
+			// ERROR
+		}
 	}
 }
