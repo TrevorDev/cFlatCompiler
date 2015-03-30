4c1afd6c6761a9efc56ce326368924f621978c47
ericboyd <epboyd@gmail.com>
2015-03-30 19:24:37 -0400

    mips generation - pass by value

--- testFiles/test.asm
+++ testFiles/test.asm
@@ -1,43 +1,52 @@
-putc:
-    move    $fp,$sp
-
-    sub     $sp,$sp,4
-    sw      $ra,4($sp)
-
-    #load and deref param
-    lw      $v0,8($fp)
-    lw      $v1,0($v0)
-
-    #display to screen
-    move    $a0,$v1
-    li      $v0, 11
-    syscall
-
-    #reset sp and fp
-    move    $sp,$fp
-    lw      $fp, 4($fp)
-
-    jr      $ra
-
+#puti function - args: int i
 puti:
+    #store ra
     move    $fp,$sp
-
     sub     $sp,$sp,4
     sw      $ra,4($sp)
 
-    #load and deref param
-    lw      $v0,8($fp)
-    lw      $v1,0($v0)
-
-    #display to screen
-    move    $a0,$v1
+    #load param
+    lw      $a0,8($fp)
     li      $v0, 1
     syscall
 
-    #reset sp and fp
+    #reset sp and fp, return
     move    $sp,$fp
     lw      $fp, 4($fp)
+    jr      $ra
 
+#putc function - args: int c
+putc:
+    #store ra
+    move    $fp,$sp
+    sub     $sp,$sp,4
+    sw      $ra,4($sp)
+
+    #load param
+    lw      $a0,8($fp)
+    li      $v0, 11
+    syscall
+
+    #reset sp and fp, return
+    move    $sp,$fp
+    lw      $fp, 4($fp)
+    jr      $ra
+
+#puti function - args: float f
+putf:
+    #store ra
+    move    $fp,$sp
+    sub     $sp,$sp,4
+    sw      $ra,4($sp)
+
+    #load param
+    l.s     $f12,8($fp)
+    li      $v0, 2
+    syscall
+
+    #reset sp and fp, return
+    move    $sp,$fp
+    lw      $fp, 4($fp)
     jr      $ra
 
 main:
@@ -46,48 +55,42 @@ main:
     sub     $sp,$sp,4
     sw      $ra,4($sp)
 
+    # push local variable on stack
     sub     $sp,$sp,4
-    li.s    $f12,1234.5678      #x = 4
-    s.s     $f12,4($sp)
+    #li.s    $f12,1234.5678
+    #s.s     $f12,4($sp)
+    li      $v0,65
+    sw      $v0,4($sp)
 
     #function call - arguments push
     sub     $sp,$sp,4
-    move    $v0,$fp
-    addi    $v0,$v0,-4
+    lw      $v0,-4($fp)
     sw      $v0,4($sp)
 
     #frame pointer push
     sub     $sp,$sp,4
     sw      $fp,4($sp)
 
-    jal     putf
+    jal     puti
 
     #stack pop
-    addi    $sp,$sp,12
+    addi    $sp,$sp,4
 
-    #jal     putc
-
-
-    lw      $ra, 0($fp)
-    jr      $ra
-
-putf:
-    move    $fp,$sp
-
+    #function call - arguments push
     sub     $sp,$sp,4
-    sw      $ra,4($sp)
+    lw      $v0,-4($fp)
+    sw      $v0,4($sp)
 
-    #load and deref param
-    lw      $a0,8($fp)
-    l.s    $f12,($a0)
+    #frame pointer push
+    sub     $sp,$sp,4
+    sw      $fp,4($sp)
 
-    #display to screen
-    #move    $a0,$v1
-    li      $v0, 2
-    syscall
+    jal     putc
+    jal     putf
+    addi    $sp,$sp,4
 
-    #reset sp and fp
-    move    $sp,$fp
-    lw      $fp, 4($fp)
 
+    #stack pop
+    addi    $sp,$sp,8
+    lw      $ra, 0($fp)
     jr      $ra
