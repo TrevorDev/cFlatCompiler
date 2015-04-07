bc5aa62b79e0143ddf0847a32a39a8e633491d08
ericboyd <epboyd@gmail.com>
2015-04-07 17:16:34 -0400

    shutup and dance and function params work with puti

--- output.asm
+++ output.asm
@@ -70,6 +70,26 @@ sub     $sp,$sp,4
 sw      $ra,4($sp)
 #push stack pointer forward for var x
 sub     $sp,$sp,4
+#push stack pointer forward for var y
+sub     $sp,$sp,4
+#push stack pointer forward for var z
+sub     $sp,$sp,4
+#Doing inline puti
+lw      $a0,-4($fp)
+li      $v0, 1
+syscall
+#Doing inline puti
+lw      $a0,-8($fp)
+li      $v0, 1
+syscall
+#Doing inline puti
+lw      $a0,-12($fp)
+li      $v0, 1
+syscall
+#Doing inline puti
+lw      $a0,-0($gp)
+li      $v0, 1
+syscall
 #reset sp and fp and return
 move    $sp,$fp
 lw      $ra, 0($fp)
@@ -89,17 +109,46 @@ sub     $sp,$sp,4
 li	$a0,5
 sw	$a0, -8($fp)
 lw	$a0,-8($fp)	#load local variable ___temp0 to register $a0
-sw	$a0, -0($gp)	#store variable ___temp0 in global variable a
+sw	$a0, -4($fp)	#store variable ___temp0 in local variable b
+#push stack pointer forward for var ___temp1
+sub     $sp,$sp,4
+li	$a0,6
+sw	$a0, -12($fp)
+lw	$a0,-12($fp)	#load local variable ___temp1 to register $a0
+sw	$a0, -0($gp)	#store variable ___temp1 in global variable a
+#Doing inline puti
+lw      $a0,-0($gp)
+li      $v0, 1
+syscall
+#push stack pointer forward for var ___temp2
+sub     $sp,$sp,4
+li	$a0,1
+sw	$a0, -16($fp)
+#push stack pointer forward for var ___temp3
+sub     $sp,$sp,4
+li	$a0,2
+sw	$a0, -20($fp)
 #frame pointer push for func call
 sub     $sp,$sp,4
 sw      $fp,4($sp)
 #store the stack pointer as it is now
 move $k0,$sp
 sub     $sp,$sp,4
-lw	$a0,-0($gp)
+sub     $sp,$sp,4
+lw	$a0,-4($fp)
+sw 	$a0,4($sp)
+sub     $sp,$sp,4
+lw	$a0,-16($fp)
+sw 	$a0,4($sp)
+sub     $sp,$sp,4
+lw	$a0,-20($fp)
 sw 	$a0,4($sp)
 move $sp,$k0 #restore stack pointer
 jal		foo
+#Doing inline puti
+lw      $a0,-0($gp)
+li      $v0, 1
+syscall
 #reset sp and fp and return
 move    $sp,$fp
 lw      $ra, 0($fp)
