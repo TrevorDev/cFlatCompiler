6d6b9e030843bf7f1de0a5900aa1e2b5ae222280
ericboyd <epboyd@gmail.com>
2015-04-07 17:31:42 -0400

    prints characters, integers. float values not propegated upwards

--- output.asm
+++ output.asm
@@ -68,28 +68,16 @@ foo:
 move    $fp,$sp
 sub     $sp,$sp,4
 sw      $ra,4($sp)
-#push stack pointer forward for var x
+#push stack pointer forward for var ericIsDome
 sub     $sp,$sp,4
-#push stack pointer forward for var y
+#Inline putc
+l.s      $f12,-4($fp)
+li      $v0, 2
+syscall
+#push stack pointer forward for var ___temp0
 sub     $sp,$sp,4
-#push stack pointer forward for var z
-sub     $sp,$sp,4
-#Doing inline puti
-lw      $a0,-4($fp)
-li      $v0, 1
-syscall
-#Doing inline puti
-lw      $a0,-8($fp)
-li      $v0, 1
-syscall
-#Doing inline puti
-lw      $a0,-12($fp)
-li      $v0, 1
-syscall
-#Doing inline puti
-lw      $a0,-0($gp)
-li      $v0, 1
-syscall
+li	$a0,1
+sw	$a0, -8($fp)
 #reset sp and fp and return
 move    $sp,$fp
 lw      $ra, 0($fp)
@@ -102,32 +90,14 @@ __main:
 move    $fp,$sp
 sub     $sp,$sp,4
 sw      $ra,4($sp)
-#push stack pointer forward for var b
+#push stack pointer forward for var c
 sub     $sp,$sp,4
-#push stack pointer forward for var ___temp0
-sub     $sp,$sp,4
-li	$a0,5
-sw	$a0, -8($fp)
-lw	$a0,-8($fp)	#load local variable ___temp0 to register $a0
-sw	$a0, -4($fp)	#store variable ___temp0 in local variable b
 #push stack pointer forward for var ___temp1
 sub     $sp,$sp,4
-li	$a0,6
-sw	$a0, -12($fp)
-lw	$a0,-12($fp)	#load local variable ___temp1 to register $a0
-sw	$a0, -0($gp)	#store variable ___temp1 in global variable a
-#Doing inline puti
-lw      $a0,-0($gp)
-li      $v0, 1
-syscall
-#push stack pointer forward for var ___temp2
-sub     $sp,$sp,4
-li	$a0,1
-sw	$a0, -16($fp)
-#push stack pointer forward for var ___temp3
-sub     $sp,$sp,4
-li	$a0,2
-sw	$a0, -20($fp)
+li.s	$f0,0.000000
+s.s	$f0, -8($fp)
+lw	$a0,-8($fp)	#load local variable ___temp1 to register $a0
+sw	$a0, -4($fp)	#store variable ___temp1 in local variable c
 #frame pointer push for func call
 sub     $sp,$sp,4
 sw      $fp,4($sp)
@@ -137,18 +107,12 @@ sub     $sp,$sp,4
 sub     $sp,$sp,4
 lw	$a0,-4($fp)
 sw 	$a0,4($sp)
-sub     $sp,$sp,4
-lw	$a0,-16($fp)
-sw 	$a0,4($sp)
-sub     $sp,$sp,4
-lw	$a0,-20($fp)
-sw 	$a0,4($sp)
 move $sp,$k0 #restore stack pointer
 jal		foo
-#Doing inline puti
-lw      $a0,-0($gp)
-li      $v0, 1
-syscall
+#push stack pointer forward for var ___temp2
+sub     $sp,$sp,4
+li	$a0,1
+sw	$a0, -12($fp)
 #reset sp and fp and return
 move    $sp,$fp
 lw      $ra, 0($fp)
