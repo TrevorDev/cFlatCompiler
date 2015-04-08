1a844367bef817564910c771beaaed5e5e580dbe
Murray Rush <rush@mail.uoguelph.ca>
2015-04-07 23:56:28 -0400

    return intval for comparison

--- output.asm
+++ output.asm
@@ -26,11 +26,23 @@ li.s	$f0,6.000000
 s.s	$f0, -4($fp)
 #push stack pointer forward for var ___temp1
 sub     $sp,$sp,4
-li.s	$f0,5.000000
+li.s	$f0,6.000000
 s.s	$f0, -8($fp)
 #push stack pointer forward for var ___temp2
 sub     $sp,$sp,4
 l.s	$f0,-4($fp)	#load global variable ___temp0 to register $f0
 l.s	$f1,-8($fp)	#load global variable ___temp1 to register $f1
-sge.s $a0,$f0,$f1
-sw	$a0, -12($fp)	#store variable ___temp2 in local variable ___temp2
+c.eq.s $f0, $f1 #FUCK
+#Inline puti
+lw      $a0,-12($fp)
+li      $v0, 1
+syscall
+#push stack pointer forward for var ___temp3
+sub     $sp,$sp,4
+li	$a0,1
+sw	$a0, -16($fp)
+#reset sp and fp and return
+move    $sp,$fp
+lw      $ra, 0($fp)
+lw      $fp, 4($fp)
+jr      $ra
